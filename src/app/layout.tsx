ac3ae747400fc9f463ebb9a520cd050e270ba2a3
narekk1202 <narekdunamaliiian@gmail.com>
2025-12-08 12:57:05 +0400

    feat: integrate Elysia for room creation and add React Query for state management
    - Added Elysia for handling room creation via a new API endpoint.
    - Integrated Redis for storing room metadata with a TTL.
    - Implemented React Query for managing room creation state in the frontend.
    - Updated layout to include Providers for React Query context.
    - Added new dependencies: @elysiajs/eden, @tanstack/react-query, and @upstash/redis.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Providers } from '@/components/providers';
 import type { Metadata } from 'next';
 import { JetBrains_Mono } from 'next/font/google';
 import './globals.css';
@@ -19,10 +20,8 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en'>
-			<body
-				className={`${jetbrainsMono.variable} antialiased`}
-			>
-				{children}
+			<body className={`${jetbrainsMono.variable} antialiased`}>
+				<Providers>{children}</Providers>
 			</body>
 		</html>
 	);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 'use client';
 
+import { client } from '@/lib/client';
+import { useMutation } from '@tanstack/react-query';
 import { nanoid } from 'nanoid';
+import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
 const ANIMALS = [
@@ -23,7 +26,15 @@ const generateUsername = () => {
 };
 
 export default function Home() {
+	const router = useRouter();
 	const [username, setUsername] = useState('Narek');
+
+	const { mutate: createRoom, isPending } = useMutation({
+		mutationFn: async () => await client.rooms.create.post(),
+		onSuccess: res => {
+			router.push('/room/' + res.data?.roomId);
+		},
+	});
 
 	useEffect(() => {
 		const main = () => {
@@ -65,7 +76,11 @@ export default function Home() {
 								</div>
 							</div>
 						</div>
-						<button className='w-full bg-zinc-100 text-black p-3 text-sm font-bold hover:bg-zinc-50 hover:text-black transition-colors mt-2 cursor-pointer disabled:opacity-50'>
+						<button
+							onClick={() => createRoom()}
+							disabled={isPending}
+							className='w-full bg-zinc-100 text-black p-3 text-sm font-bold hover:bg-zinc-50 hover:text-black transition-colors mt-2 cursor-pointer disabled:opacity-50'
+						>
 							CREATE SECURE ROOM
 						</button>
 					</div>
